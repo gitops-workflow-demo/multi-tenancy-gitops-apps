2d682f7a878dd6756d6aacefb035ab8901f68992
hollisc <hollisc@ca.ibm.com>
2021-09-14 13:13:47 -0400

    Update mq-kubeseal.sh
    Signed-off-by: hollisc <hollisc@ca.ibm.com>

--- scripts/mq-kubeseal.sh
+++ scripts/mq-kubeseal.sh
@@ -14,7 +14,6 @@ popd () {
     command popd "$@" > /dev/null
 }
 
-SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
 
 if [ -z ${GIT_USER} ]; then echo "Please set GIT_USER when running script"; exit 1; fi
 
@@ -24,6 +23,13 @@ if [ -z ${GIT_ORG} ]; then echo "Please set GIT_ORG when running script"; exit 1
 
 #if [ -z ${ARTIFACTORY_NEW_PASSWORD} ]; then echo "Please set ARTIFACTORY_NEW_PASSWORD when running script"; exit 1; fi
 
+
+SKIP_ARGO_REPLACE_GIT=${SKIP_ARGO_REPLACE_GIT:-true}
+SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
+GIT_HOST=${GIT_HOST:-github.com}
+GIT_BASEURL="https://${GIT_HOST}"
+GITOPS_REPO=${GITOPS_REPO:-multi-tenancy-gitops-apps}
+GITOPS_BRANCH=${GITOPS_BRANCH:-ocp47-2021-2}
 
 wait_kubeseal_ready () {
     while ! oc wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n ${SEALED_SECRET_NAMESPACE} > /dev/null; do sleep 30; done
@@ -39,6 +45,27 @@ mq_kubeseal_git () {
     pushd $ROOTDIR/mq/environments/ci/secrets
     source git-credentials-secret.sh
     popd
+}
+
+mq_update_git () {
+
+    find ${ROOTDIR}/mq/environments -name '*.yaml' -print0 |
+    while IFS= read -r -d '' File; do
+        echo "Processing $File"
+        sed -i'.bak' -e "s#https://github.com/cloud-native-toolkit-demos/multi-tenancy-gitops-apps.git#${GIT_BASEURL}/${GIT_ORG}/${GITOPS_REPO}#" $File
+        rm "${File}.bak"
+    done
+
+    if [[ "${SKIP_ARGO_REPLACE_GIT}" == "false" ]]; then
+        find ${ROOTDIR}/mq/config/argocd -name '*.yaml' -print0 |
+        while IFS= read -r -d '' File; do
+            echo "Processing $File"
+            sed -i'.bak' -e "s#https://github.com/cloud-native-toolkit-demos/multi-tenancy-gitops-apps.git#${GIT_BASEURL}/${GIT_ORG}/${GITOPS_REPO}#" $File
+            sed -i'.bak' -e "s#targetRevision: master#targetRevision: ${GITOPS_BRANCH}#" $File
+            rm "${File}.bak"
+        done
+    fi
+
 }
 
 mq_gitops_repo_cm () {
@@ -68,6 +95,8 @@ wait_kubeseal_ready
 mq_kubeseal_artifactory
 
 mq_kubeseal_git
+
+mq_update_git
 
 mq_gitops_repo_cm
 
